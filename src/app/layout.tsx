c851cda1a6a373163cfbdb582daae20bd23e50dc
taiquyqt <132183408+taiquyqt@users.noreply.github.com>
2025-07-23 16:21:11 +0700

    cmt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { SessionProviderWrapper } from "@/components/SessionProviderWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="vi">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body className={inter.className}>
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
+      </body>
     </html>
   );
 }
