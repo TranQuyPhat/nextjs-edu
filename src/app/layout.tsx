c5815e2128647897513053b1fadf2d96be3ce65c
chintp2308 <nphuongchi23@gmail.com>
2025-07-23 16:37:30 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProviderWrapper } from "@/components/SessionProviderWrapper";
-import Footer from "@/components/footer/page"
+import Footer from "@/components/footer/page";
 
 
 const inter = Inter({ subsets: ["latin"] });
