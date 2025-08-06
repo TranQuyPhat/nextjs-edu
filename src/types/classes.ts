290416c9e33362df42b79650c81a6025e7c156c7
Ngoc Nhan <nhanno1st@gmail.com>
2025-08-06 15:45:23 +0700

    add axios

---
+++ src/types/classes.ts
@@ -0,0 +1,10 @@
+export interface ClassItem {
+  id: number;
+  className: string;
+  schoolYear: number;
+  semester: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  teacherId: number;
+}
