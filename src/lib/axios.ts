0302046f2625b4a453c2cbf859bb6657fb54049a
chintp2308 <nphuongchi23@gmail.com>
2025-08-13 11:14:28 +0700

    Feature/logingoogle (#5)
    * feat: hoàn thành tính năng login-register
    
    * feat: hoàn thành tính năng task1

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,25 +1,27 @@
 // lib/apiClient.ts
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-  timeout: 5000,
+  timeout: 10000,
 });
 
 // Optional: Thêm interceptor nếu cần xử lý token
-// apiClient.interceptors.request.use(
-//   (config) => {
-//     // Ví dụ: nếu có token trong localStorage
-//     const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+
+apiClient.interceptors.request.use(
+  (config) => {
+    // Ví dụ: nếu có token trong localStorage
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default apiClient;
