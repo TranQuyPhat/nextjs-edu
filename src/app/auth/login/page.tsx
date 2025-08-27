c8fdf4fc037e30f367d53998cd843ac7e63e9685
chintp2308 <nphuongchi23@gmail.com>
2025-08-27 20:20:25 +0700

    updateProfileAvatar (#12)
    * profile
    
    * updateProfileAvatar

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -144,7 +144,6 @@ export default function LoginPage() {
       if (res.roles && res.roles.length === 1) {
         const role = res.roles[0].toLowerCase();
         localStorage.setItem("role", role);
-        localStorage.setItem("role", role);
         router.push(`/dashboard/${role}`);
       } else {
         router.push("/select-role");
